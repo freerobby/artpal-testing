874163071d6a001c37c4b6674e3966c1d296ce45
Robby Grossman <robby@freerobby.com>
2010-02-05 23:20:47 -0500

    Test htmlspecialchars_decode() for PHP4 compatibility.

--- test_plugin.php
+++ test_plugin.php
@@ -1,8 +1,13 @@
 <?php
 
+require_once 'artpal/artpal.php';
+
 class TestPlugin extends WPTestCase {
-  function test_callable() {
-    $this->assertEquals("true", "true");
+  /*
+    Verify that we have successfully defined this function in PHP4.
+  */
+  function test_function_htmlspecialchars_decode() {
+    $this->assertEquals(htmlspecialchars_decode("This&gt;that"), "This>that");
   }
 }
 
