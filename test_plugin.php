94b5d10fc5e30aecea5380b81cb7a5e6f07a5c3d
Robby Grossman <robby@freerobby.com>
2010-02-05 23:21:26 -0500

    Verify we've defined all our constants.

--- test_plugin.php
+++ test_plugin.php
@@ -9,6 +9,23 @@ class TestPlugin extends WPTestCase {
   function test_function_htmlspecialchars_decode() {
     $this->assertEquals(htmlspecialchars_decode("This&gt;that"), "This>that");
   }
+  
+  /*
+    We've defined all our constants.
+  */
+  function test_constants_defined() {
+    $constants = array(
+      ds_ap_CFPRICE,
+      ds_ap_CFSHIPPING,
+      ds_ap_TAGINSERT,
+      ds_ap_TAGIPN
+    );
+    foreach($constants as $constant) {
+      echo $constant;
+      $this->assertTrue($constant != null);
+      $this->assertTrue($constant != "");
+    }
+  }
 }
 
 ?>
